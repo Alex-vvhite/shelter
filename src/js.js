2f02a520f9a6fc44849c2670d4307eeb03368b9b
Aleksei <xodyrev.aleksej42@gmail.com>
2022-04-28 00:23:12 +0300

    feat: fix burget menu

--- src/js.js
+++ src/js.js
@@ -1,8 +1,9 @@
 const burger = document.querySelector('.hamburger');
+const menu = document.querySelector('.header-menu');
+const header_logo = document.querySelector('.header_items');
+const items_logo = document.querySelector('.item_logo');
+const burger_color = document.querySelector('colo-burger');
 if (burger){
-    const menu = document.querySelector('.header-menu');
-    const header_logo = document.querySelector('.header_items');
-    const items_logo = document.querySelector('.item_logo');
     burger.addEventListener("click", ()=>{
         document.body.classList.toggle('_lock');
         burger.classList.toggle('active');
@@ -10,4 +11,16 @@ if (burger){
         header_logo.classList.toggle('active-header');
         items_logo.classList.toggle('active-item');
     })
+
 }
+
+if(burger.classList.contains('active')){
+    menu.classList.remove('active-menu');
+    header_logo.classList.remove('active-header');
+    items_logo.classList.remove('active-item');
+    document.body.classList.remove('_lock');
+}
+
+
+
+
